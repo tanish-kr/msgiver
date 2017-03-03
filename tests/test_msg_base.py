0c553f77224721a136a684dacd4eee49f4ddb936
kitaro-tn <tora.1986.tatsu@gmail.com>
2017-03-03 12:45:18 +0900

    added local CI

---
+++ tests/test_msg_base.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+class TestMsgBase(unittest.TestCase):
+
+    def test_sample(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
