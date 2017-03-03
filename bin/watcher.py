3413fc82577b89fcd5301fc961696d4fec4d0780
Tatsunori Nishikori <tora.1986.tatsu@gmail.com>
2017-03-03 12:52:48 +0900

    clean comment
    modify exception log

--- bin/watcher.py
+++ bin/watcher.py
@@ -18,22 +18,18 @@ class CIHandler(FileSystemEventHandler):
         self.context = context
 
     def on_created(self, event):
-        # test(self.context)
         test(event)
 
     def on_modified(self, event):
-        # test(self.context)
         test(event)
 
 
 def test(context):
     try:
         if not context.is_directory and re.compile(".py$").search(context.src_path):
-            # pep8
             logging.info("Static code analysis with pep8 :%s", context.src_path)
             subprocess.call(["pep8", context.src_path])
             logging.info("Unit test :%s", context.src_path)
-            # unittest
             test_file_name = "test_" + context.src_path.split("/")[-1]
             test_file_path = os.path.join(current_path(), "tests", test_file_name)
             if os.path.exists(test_file_path):
@@ -41,7 +37,8 @@ def test(context):
             else:
                 logging.warn("No such file %s", test_file_path)
 
-    except Exception:
+    except Exception as err:
+        logging.exception("Error dosomething: %s", err)
         pass
 
 
@@ -56,9 +53,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d%H:%M:%S')
-    # path = sys.argv[1] if len(sys.argv) > 1 else '.'
     path = current_path() + "/msgiver"
-    # event_handler = LoggingEventHandler()
     event_handler = CIHandler(path)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
