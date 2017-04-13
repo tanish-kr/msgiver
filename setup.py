6f2b9e473acbbaa1aea7edf8d0896a01197291a2
kitaro-tn <tora.1986.tatsu@gmail.com>
2017-04-13 17:14:29 +0900

    fix python 2.7 input

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open("README.rst") as readme_file:
 
 setup(
         name="msgiver",
-        version="0.1.4",
+        version="0.1.5",
         url="https://github.com/kitaro-tn/msgiver",
         scripts=["bin/msgiver"],
         license="MIT",
